e2955972b2c56895562f7e20c91963a05353ab52
neeteshdixit <dixitneetesh857@gmail.com>
2026-04-04 12:16:15 +0530

    remove unused route for 'Learn' in AppRoutes component

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,7 +3,6 @@ import Home from './pages/Home.tsx'
 import Clean from './pages/Clean.tsx'
 import Train from './pages/Train.tsx'
 import Results from './pages/Results.tsx'
-import Learn from './pages/Learn.tsx'
 
 export default function AppRoutes() {
   return (
@@ -12,7 +11,6 @@ export default function AppRoutes() {
       <Route path="/clean" element={<Clean />} />
       <Route path="/train" element={<Train />} />
       <Route path="/results" element={<Results />} />
-      <Route path="/learn" element={<Learn />} />
     </Routes>
   )
 }
